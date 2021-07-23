6803ef5811e93a55d96ab8e519983888557c01f7
André Kuratomi <andrekuratomi@gmail.com>
2021-07-23 19:21:30 -0300

    basic libs downloaded, raw components created as well as pages, routes and globalStyles

---
+++ src/components/GlobalStyles/index.jsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    body, main, form, input, div, button, p, h1, h2,h3 {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :root {
+        
+    }
+`;
