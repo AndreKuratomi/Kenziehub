b2a79a5a4785f4285c1d26433da42956c5a02be4
André Kuratomi <andrekuratomi@gmail.com>
2021-07-24 00:15:52 -0300

    Little adjust to component Cards

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,14 +1,14 @@
 export const Cards = ({ cardPlace, handleCardplace }) => {
   return (
-    <>
+    <ul>
       {cardPlace &&
         cardPlace.map((elt, index) => (
-          <article key={index}>
+          <li key={index}>
             <h3>Tecnologia</h3>
             <p>{elt}</p>
             <button onClick={handleCardplace(elt)}>Deletar</button>
-          </article>
+          </li>
         ))}
-    </>
+    </ul>
   );
 };
