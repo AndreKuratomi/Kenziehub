5b7605bb9689e6d25b8c56f1f41db7d8bb1beece
André Kuratomi <andrekuratomi@gmail.com>
2021-07-25 23:45:14 -0300

    Minimum Dashboard style made

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -3,35 +3,37 @@ import { Redirect } from "react-router-dom";
 
 import Cards from "../../components/Cards";
 
+import { Container, Input, Button } from "./styles";
+
 function Dashboard({ authenticated }) {
+  const [value, setValue] = useState("");
+  const [cardPlace, setCardPlace] = useState([]);
+
+  const addCards = (item) => {
+    setCardPlace(...cardPlace, item);
+  };
+
+  const handleCardplace = (clickedItem) => {
+    cardPlace.filter((elt) => elt !== clickedItem);
+  };
+
   if (!authenticated) {
     return <Redirect to="/login" />;
   }
 
-  // const [value, setValue] = useState("");
-  // const [cardPlace, setCardPlace] = useState([]);
-
-  // const addCards = (item) => {
-  //   setCardPlace(...cardPlace, item);
-  // };
-
-  // const handleCardplace = (clickedItem) => {
-  //   cardPlace.filter((elt) => elt !== clickedItem);
-  // };
-
   return (
-    <>
+    <Container>
       <h1>Dashboard!</h1>
-      {/* <section>
-        <input
+      <section>
+        <Input
           placeholder="Cadastre suas tecnologias"
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
-        <button onClick={() => addCards(value)}>Adicionar</button>
+        <Button onClick={() => addCards(value)}>Adicionar</Button>
         <Cards cardPlace={cardPlace} handleCardplace={handleCardplace} />
-      </section> */}
-    </>
+      </section>
+    </Container>
   );
 }
 
